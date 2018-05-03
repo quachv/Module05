9dc433b205589655dab1cd532a027f9f31450106
quachv <38046552+quachv@users.noreply.github.com>
2018-05-03 08:58:44 -0700

    java edit

--- java.js
+++ java.js
@@ -1,14 +1,11 @@
-var table = 3;             // Unit of table
+var table = prompt("Please enter a number from 1 to 10";             // Unit of table
 var operator = 'multiplication'; // Type of calculation
 var i = 1;                 // Set counter to 1
 var msg = '';              // Message
 
-if (operator == 'multiplication') {
-  // Do addition
-  while (i < 11) {
-    msg += i + ' x ' + table + ' = ' + (i * table) + '<br />';
+if (table < 11)) {
+	msg += i + ' x ' + table + ' = ' + (i * table) + '<br />';
     i++;
-  }
 }
 
 // Write the message into the page
