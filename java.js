0e06ce028a499b36404134bf7e3c44a0b1d40c74
quachv <38046552+quachv@users.noreply.github.com>
2018-05-03 09:03:46 -0700

    java edit

--- java.js
+++ java.js
@@ -3,9 +3,12 @@ var operator = 'multiplication'; // Type of calculation
 var i = 1;                 // Set counter to 1
 var msg = '';              // Message
 
-if (table < 11)) {
-	msg += i + ' x ' + table + ' = ' + (i * table) + '<br />';
+if (operator == 'multiplication') {
+  // Do addition
+  while (i < 11) {
+    msg += i + ' x ' + table + ' = ' + (i * table) + '<br />';
     i++;
+  }
 }
 
 // Write the message into the page
